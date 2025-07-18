fb8655f794ae70099b47a0a8f63b292f23ebc810
황정빈 <hjbin1211@gmail.com>
2025-07-18 22:45:47 +0900

    Request Scope

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,7 +1,11 @@
 package hello.core.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @Scope(value = "request")
@@ -16,5 +20,16 @@ public class MyLogger {
 
     public void log(String message) {
         System.out.println("[" + uuid + "][" + requestURL + "]" + " [" + message + "]");
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
     }
 }
